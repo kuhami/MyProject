bd9cdb6480f7e390e95af36bf94f54b980f6f512
liu.yalei <liu.yalei@puscene.com>
2018-10-08 16:49:32 +0800

    php

--- php/studyPHP/demo2.php
+++ php/studyPHP/demo2.php
@@ -14,6 +14,15 @@
  }else{
 
  }
+echo '<hr>';
+
+ $cars=array("Volvo","BMW","Toyota");
+ echo $cars[0];
+
+ echo '<hr>';
+ $arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+
+echo json_encode($arr);
 
 
 
