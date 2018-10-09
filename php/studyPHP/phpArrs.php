2eabbea8953e76a6352099a991a1debcd9d77132
liu.yalei <liu.yalei@puscene.com>
2018-10-09 16:06:38 +0800

    php

---
+++ php/studyPHP/phpArrs.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyl
+ * Date: 2018/10/9
+ * Time: 下午4:05
+ */
+
+$cars = array
+(
+    array("Volvo",100,96),
+    array("BMW",60,59),
+    array("Toyota",110,100)
+);
