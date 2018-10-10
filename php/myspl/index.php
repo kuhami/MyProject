de9ab19638894cc41c648de4dd7459961fe71a6d
liu.yalei <liu.yalei@puscene.com>
2018-10-10 14:04:28 +0800

    change sql

--- php/myspl/index.php
+++ php/myspl/index.php
@@ -2,24 +2,70 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
+$dbname = "myDB";
 $port = "8889";
 // 创建连接
-$conn = new mysqli($servername, $username, $password, "",$port);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // 检测连接
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
+if (!$conn) {
+    die("连接失败: " . mysqli_connect_error());
 }
-echo "连接成功";
 
-// 创建数据库
-$sql = "CREATE DATABASE myDB";
+// 使用 sql 创建数据表
+
+// 使用 sql 创建数据表
+//$sql = "CREATE TABLE MyGuests (
+//id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+//firstname VARCHAR(30) NOT NULL,
+//lastname VARCHAR(30) NOT NULL,
+//email VARCHAR(50),
+//reg_date TIMESTAMP
+//)";
+//
+//if ($conn->query($sql) === TRUE) {
+//    echo "Table MyGuests created successfully";
+//} else {
+//    echo "创建数据表错误: " . $conn->error;
+//}
+//
+
+/*$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+echo $conn->query($sql) === TRUE;
+
 if ($conn->query($sql) === TRUE) {
-    echo "数据库创建成功";
+    echo "新记录插入成功";
 } else {
-    echo "Error creating database: " . $conn->error;
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}*/
+//$conn->close();
+
+//$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+//VALUES ('John', 'Doe', 'john@example.com');";
+//$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+//VALUES ('Mary', 'Moe', 'mary@example.com');";
+//$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+//VALUES ('Julie', 'Dooley', 'julie@example.com')";
+//
+//
+//if ($conn->multi_query($sql) === TRUE) {
+//    echo "新记录插入成功";
+//} else {
+//    echo "Error: " . $sql . "<br>" . $conn->error;
+//}
+
+$sql = mysqli_query($conn,"DELETE FROM MyGuests WHERE LastName='Dooley'");
+
+
+if ($sql) {
+    echo "删除成功";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$conn->close(); //关闭链接
+$conn->close();
+
 
 ?>
